971464a86f668727cf6970f2faf10445327cdbb3
Ross Anderson <ross.w.anderson@gmail.com>
2012-05-02 17:39:40 -0400

    Major update to add 3D terrain

---
+++ Source/RAShaderProgram.h
@@ -0,0 +1,13 @@
+//
+//  RAShader.h
+//  EarthViewExample
+//
+//  Created by Ross Anderson on 4/28/12.
+//  Copyright (c) 2012 Ross Anderson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RAShader : NSObject
+
+@end
---
+++ Source/RAShaderProgram.m
@@ -0,0 +1,13 @@
+//
+//  RAShader.m
+//  EarthViewExample
+//
+//  Created by Ross Anderson on 4/28/12.
+//  Copyright (c) 2012 Ross Anderson. All rights reserved.
+//
+
+#import "RAShader.h"
+
+@implementation RAShader
+
+@end
